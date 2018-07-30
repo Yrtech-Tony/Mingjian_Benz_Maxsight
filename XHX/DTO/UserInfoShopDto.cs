ba575c50c584d89705c36da939d3b777a592b5f7
Tony <moujunsheng15@163.com>
2018-07-30 20:30:46 +0800

    发

---
+++ XHX/DTO/UserInfoShopDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace XHX.DTO
+{
+    public class UserInfoShopDto
+    {
+        public string ProjectCode { get; set; }
+        public string ShopCode { get; set; }
+        public string UserId { get; set; }
+        public string ShopName { get; set; }
+        public string InUserId { get; set; }
+        public DateTime? InDateTime { get; set; }
+
+        private char _statusType = StatusTypes.NONE;
+        public char StatusType { get { return _statusType; } set { _statusType = value; } }
+    }
+}
